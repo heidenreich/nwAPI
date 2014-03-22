d8d9b6acbd425d3b2a8a279115dc6a55c7d60c8d
Todd Heidenreich <jtheidenreich@gmail.com>
2014-03-22 17:15:00 -0400

    Adding copy

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,66 +1,47 @@
 console.log('its chart time');
 
-function assignValues (nextData) {
-	return [
-		{
-			value: Number(nextData[0].temperature),
-			color: '#AFFF00'
-		},
-		{
-			value: Number(nextData[1].temperature),
-			color: '#E89008'
-		},
-		{
-			value: Number(nextData[2].temperature),
-			color: '#FF044A'
-		},
-		{
-			value: Number(nextData[3].temperature),
-			color: '#1400E8'
-		},
-		{
-			value: Number(nextData[4].temperature),
-			color: '#09FFCF'
-		},
-		{
-			value: Number(nextData[5].temperature),
-			color: '#584A5E'
-		}
-	];
+function assignValues(nextData) {
+   return [{
+      value: Number(nextData[0].temperature),
+      color: '#AFFF00'
+   }, {
+      value: Number(nextData[1].temperature),
+      color: '#E89008'
+   }, {
+      value: Number(nextData[2].temperature),
+      color: '#FF044A'
+   }, {
+      value: Number(nextData[3].temperature),
+      color: '#1400E8'
+   }, {
+      value: Number(nextData[4].temperature),
+      color: '#09FFCF'
+   }, {
+      value: Number(nextData[5].temperature),
+      color: '#FFFA00'
+   }];
 }
 
+$.get('http://tiny-pizza-server.herokuapp.com/collections/weather', function(response) {
+   var nextData = response;
+   console.log(nextData[9]);
+   var data = assignValues(nextData);
 
+   //-- Make the chart once you have the data
 
-$.get('http://tiny-pizza-server.herokuapp.com/collections/weather',function(response){
-	var nextData = response;
-	console.log(nextData[9]);
-	var data = assignValues(nextData);
+   var options = {
+      animation: true,
+      scaleOverride: true,
+      scaleSteps: 30,
+      scaleStepWidth: 0.5,
+      scaleStartValue: 60
+   };
 
-	//-- Make the chart once you have the data
+   var c = $('#myChart');
+   var ct = c.get(0).getContext('2d');
+   var ctx = document.getElementById('myChart').getContext('2d');
 
-	var options = {
-		animation: true,
-		scaleOverride: true,
-		scaleSteps: 30,
-		scaleStepWidth: 0.5,
-		scaleStartValue: 60
-	};
+   console.log(data);
 
-	var c = $('#myChart');
-	var ct = c.get(0).getContext('2d');
-	var ctx = document.getElementById('myChart').getContext('2d');
-	
-	console.log(data);
-
-	new Chart(ct).PolarArea(data, options);
+   new Chart(ct).PolarArea(data, options);
 });
-
-    
-
-
-
-
-
-
-
-
