5e03f15a92e1e64ccd30332c0cfbf5c873c9e6bb
Todd Heidenreich <jtheidenreich@gmail.com>
2014-03-28 08:23:09 -0400

    changed chart type

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,28 +1,30 @@
 console.log('its chart time');
 
+
 function assignValues(nextData) {
-    return [{
-        value: parseInt(_.find(nextData, function(weather) {
-            return weather.location === 'conference room';
-        }).temperature),
-        color: '#AFFF00'
-    }, {
-        value: parseInt(_.find(nextData, function(weather) {
-            return weather.location === 'lobby';
-        }).temperature),
-        color: '#E89008'
-    }, {
-        value: parseInt(_.find(nextData, function(weather) {
-            return weather.location === 'lecture hall';
-        }).temperature),
-        color: '#FF044A'
-    }, {
-        value: parseInt(_.find(nextData, function(weather) {
-            return weather.location === 'workspace de Todd';
-        }).temperature),
-        color: '#1400E8'
-    }];
+    return {
+        labels: ["lecture hall", "conference room", "lobby", "workspace de Todd"],
+        datasets: [{
+            fillColor: "rgba(255,152,0,1)",
+            strokeColor: "rgba(220,220,220,1)",
+            data: [
+                parseInt(_.find(nextData, function(weather) {
+                    return weather.location === 'lecture hall';
+                }).temperature),
+                parseInt(_.find(nextData, function(weather) {
+                    return weather.location === 'conference room';
+                }).temperature),
+                parseInt(_.find(nextData, function(weather) {
+                    return weather.location === 'lobby';
+                }).temperature),
+                parseInt(_.find(nextData, function(weather) {
+                    return weather.location === 'workspace de Todd';
+                }).temperature)
+            ]
+        }]
+    }
 }
+
 
 $.get('http://tiny-pizza-server.herokuapp.com/collections/weather', function(response) {
     var nextData = response;
@@ -34,10 +36,12 @@ $.get('http://tiny-pizza-server.herokuapp.com/collections/weather', function(res
 
     var options = {
         animation: true,
-        // scaleOverride: true,
-        // scaleSteps: 16,
-        // scaleStepWidth: 0.5,
-        // scaleStartValue: 62
+        scaleOverlay: true,
+        scaleFontColor: "#fff",
+        barValueSpacing: 5,
+        barDatasetSpacing: 1,
+        scaleFontFamily: "'Open Sans'"
+
     };
 
     var c = $('#myChart');
@@ -46,27 +50,5 @@ $.get('http://tiny-pizza-server.herokuapp.com/collections/weather', function(res
 
     console.log(data);
 
-    new Chart(ct).PolarArea(data, options);
+    new Chart(ct).Bar(data, options);
 });
-
-
-
-
-// new Chart(ctx).Bar(data,options);
-
-
-// var data = {
-//    labels : ["lecture hall","downstairs","next lobby","work space"],
-//    datasets : [
-//       {
-//          fillColor : "rgba(220,220,220,0.5)",
-//          strokeColor : "rgba(220,220,220,1)",
-//          data : [65,59,90,81,56,55,40]
-//       },
-//       {
-//          fillColor : "rgba(151,187,205,0.5)",
-//          strokeColor : "rgba(151,187,205,1)",
-//          data : [28,48,40,19,96,27,100]
-//       }
-//    ]
-// }
